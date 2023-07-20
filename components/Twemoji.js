d79fe3414f8d589b29f7d02d5ecced757966317c
Dilip Kumar <curiousdilip@gmail.com>
2023-07-20 12:21:53 +0530

    48px by 48px

--- components/Twemoji.js
+++ components/Twemoji.js
@@ -2,7 +2,7 @@ import kebabCase from '@/lib/utils/kebabCase'
 
 const Twemoji = ({ emoji, size = 'twa-lg', className }) => {
   const twaClass = kebabCase(emoji)
-  return <i className={`twa ${size} twa-${twaClass} ${className}`} />
+  return <i className={`twa ${size} twa-${twaClass} ${className}`} width="48px" height="48px" />
 }
 
 export default Twemoji
